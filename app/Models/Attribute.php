2e3fc0acdc84e63f1b94c84183530c60fa417519
hoangson <sonht2219@gmail.com>
2021-06-11 23:29:00 +0700

    comment model units, attributes

--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -5,6 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Attribute
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Attribute newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Attribute newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Attribute query()
+ * @mixin \Eloquent
+ */
 class Attribute extends Model
 {
     use HasFactory;
--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -5,6 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Unit
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Unit newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Unit newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Unit query()
+ * @mixin \Eloquent
+ */
 class Unit extends Model
 {
     use HasFactory;
